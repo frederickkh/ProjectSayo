06f5ba8f156700c549ecad6dd93e4d47cb78ca45
Frederick <frederick.kh2003@gmail.com>
2026-03-24 15:46:01 +0800

    Changed similarity_threshold

--- backend/chat_api.py
+++ backend/chat_api.py
@@ -184,7 +184,7 @@ def retrieve_relevant_documents(
             "search_documents",
             {
                 "query_embedding": embedding,
-                "similarity_threshold": 0.1,
+                "similarity_threshold": 0.4,
                 "match_count": RAG_CONTEXT_LIMIT,
                 "manual_type_filter": manual_type
             }
